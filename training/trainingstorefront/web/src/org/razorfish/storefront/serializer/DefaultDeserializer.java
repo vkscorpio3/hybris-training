7abe2dd2299c99df99fe16a663f403302221b261
ricky.li <281423889@qq.com>
2016-06-01 16:04:53 +0800

    no message

---
+++ training/trainingstorefront/web/src/org/razorfish/storefront/serializer/DefaultDeserializer.java
@@ -0,0 +1,33 @@
+package org.razorfish.storefront.serializer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import org.springframework.core.NestedIOException;
+import org.springframework.core.serializer.Deserializer;
+import org.springframework.core.ConfigurableObjectInputStream;
+
+public class DefaultDeserializer implements Deserializer<Object> {
+    private final ClassLoader classLoader;
+
+    public DefaultDeserializer() {
+        this.classLoader = null;
+    }
+
+    public DefaultDeserializer(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    @SuppressWarnings("resource")
+    public Object deserialize(InputStream inputStream) throws IOException {
+        ObjectInputStream objectInputStream = new ConfigurableObjectInputStream(inputStream, classLoader);
+        try {
+            return objectInputStream.readObject();
+        }
+        catch (ClassNotFoundException ex) {
+            throw new NestedIOException("Failed to deserialize object type", ex);
+        }
+    }
+
+}
